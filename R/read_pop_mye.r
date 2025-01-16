9cd16fc38dddc0c925ba548106e523915399e9e2
paulseamer <paulseamer@nhs.net>
2025-01-16 10:00:45 +0000

    Read population estimates

---
+++ R/read_pop_mye.r
@@ -0,0 +1,52 @@
+# README
+# Read-in mid-year population estimates for years 2000 to 2023
+# https://www.nomisweb.co.uk
+# MYE for 2000 are 0-85+. We use England numbers from 2001 to apportion 85+
+# population in 2000 to ages 85-90+.
+
+# read LADs ----
+process_mye_lad <- function(filenm, skip = 6, na = c("", NA, "-")) {
+
+  start_year <- 1991
+  end_year   <- 2023
+  grp_85plus <- start_year:2000
+  grp_90plus <- 2001:end_year
+
+  readr::read_csv(filenm, skip = skip, na = na) |>
+    dplyr::rename(
+      area_name = Area,
+      area_code = mnemonic,
+      all_ages = `All Ages`,
+      row_id = row
+    ) |>
+    dplyr::filter(
+      !is.na(area_name),
+      stringr::str_detect(area_code, "^E0|^E92")
+    ) |>
+    dplyr::mutate(row_id = as.integer(row_id)) |>
+    dplyr::mutate(
+      tbl_id = cumsum(row_id == 1),
+      year = dplyr::if_else(tbl_id %% 2 == 1, (tbl_id + 1) / 2, tbl_id / 2),
+      year = year + start_year - 1,
+      sex = dplyr::if_else(tbl_id %% 2 == 1, "m", "f"),
+    ) |>
+    dplyr::select(-c(row_id, tbl_id, area_name, all_ages)) |>
+    tidyr::pivot_longer(
+      tidyselect::starts_with("Age"),
+      names_to = "age",
+      values_to = "pop"
+    ) |>
+    dplyr::mutate(age = stringr::str_extract(age, "[0-9].*$")) |>
+    dplyr::mutate(
+      across(c("year"), as.integer),
+      pop = as.double(pop)
+    ) |>
+    dplyr::mutate(
+      grp = dplyr::case_when(
+        year %in% grp_85plus ~ "85plus",
+        year %in% grp_90plus ~ "90plus"
+      )
+    ) |>
+    # split on upper age group (85+/90+)
+    dplyr::group_split(grp)
+}
--- _targets.r
+++ _targets.r
@@ -19,12 +19,16 @@ tar_source(
     here::here("R", "read_very_old.r"),
     here::here("R", "read_life_tables_2018b.r"),
     here::here("R", "read_snpp_2018b.r"),
-    here::here("R", "read_npp_2018b.r")
+    here::here("R", "read_npp_2018b.r"),
+    here::here("R", "read_pop_mye.r")
   )
 )
 
 # target list ----
 list(
+  #############################################################################
+  # read-in population data
+  #############################################################################
   tar_target(data_raw_very_old, here::here("data_raw", "englandevo2023.csv"),
     format = "file"
   ),
@@ -40,7 +44,7 @@ list(
       full.names = TRUE
     ),
   ),
-  tar_target(df_lifetbl, process_life_tables(lt_paths), pattern = map(lt_paths)),
+  tar_target(df_lifetbl, process_life_tables(lt_paths), pattern = map(lt_paths)), # nolint: line_length_linter.
   # branch over snpp variants
   tar_files(
     snpp_paths,
@@ -63,8 +67,15 @@ list(
     ),
   ),
   tar_target(df_npp, process_npp(npp_paths), pattern = map(npp_paths)),
-  tar_target(data_raw_npp_codes, here::here("data_raw", "npp_2018b", "NPP codes.txt"),
+  tar_target(data_raw_npp_codes, here::here("data_raw", "npp_2018b", "NPP codes.txt"), # nolint: line_length_linter.
     format = "file"
   ),
-  tar_target(df_npp_codes, process_npp_codes(data_raw_npp_codes, "npp_2018b_codes.csv"))
+  tar_target(df_npp_codes, process_npp_codes(data_raw_npp_codes, "npp_2018b_codes.csv")), # nolint: line_length_linter.
+  tar_target(data_raw_mye_lad, here::here("data_raw", "nomis_mye_lad_1991to2023_20250116.csv"), # nolint: line_length_linter.
+    format = "file"
+  ),
+  tar_target(df_mye_lad, process_mye_lad(data_raw_mye_lad))
+  #############################################################################
+  # read-in xxx
+  #############################################################################
 )
