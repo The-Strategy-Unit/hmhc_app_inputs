e50bd8cb800c69abea433981e3dd9cc108025182
paulseamer <paulseamer@nhs.net>
2025-01-31 17:01:11 +0000

    Compare local activity rates with national rates

--- R/assemble_activity_inputs.r
+++ R/assemble_activity_inputs.r
@@ -1,14 +1,8 @@
 # README
 # assemble data files for activity profiles interactive viz (JSON format)
 
-# TODO
-# think about base_year argument in observed profiles
-# think about area_codes argument in format_profiles_json
-# incorporate code for generating review plots into pipeline
-source(here::here("R", "hsa_helper_fns.r")) # path_closure()
-source(here::here("R", "hsa_create_gams.r")) # run_area_gams()
-
 # functions ----
+# create_obs_rt_df_all_areas
 # run_gams_all_areas
 # get_observed_profiles
 # get_modeled_profiles
@@ -26,7 +20,16 @@ if (!rlang::is_logical(test)) {
   area_codes <- test_areas
 }
 
-# run_gams_all_areas ----
+# create_obs_rt_df_all_areas() ----
+create_obs_rt_df_all_areas <- function(area_codes, base_year) {
+  purrr::map(
+    area_codes, \(x) {
+      create_obs_rt_df(x, base_year)
+    }
+  )
+}
+
+# run_gams_all_areas() ----
 run_gams_all_areas <- function(area_codes, base_year) {
   purrr::map(
     area_codes, \(x) {
@@ -35,13 +38,13 @@ run_gams_all_areas <- function(area_codes, base_year) {
   )
 }
 
-# get_observed_profiles ----
+# get_observed_profiles() ----
 get_observed_profiles <- function(area_codes, base_year) {
   profiles_ls <- purrr::map(
     area_codes, \(x) {
       path_self <- path_closure(x, base_year)
       readr::read_csv(
-        path_self("model_rt_df.csv"),
+        path_self("obs_rt_df.csv"),
         show_col_types = FALSE
       )
     }
@@ -50,7 +53,7 @@ get_observed_profiles <- function(area_codes, base_year) {
   dplyr::bind_rows(profiles_ls)
 }
 
-# get_modeled_profiles ----
+# get_modeled_profiles() ----
 get_modeled_profiles <- function(area_codes, base_year) {
   profiles_ls <- purrr::map(
     area_codes, \(x) {
@@ -65,7 +68,7 @@ get_modeled_profiles <- function(area_codes, base_year) {
   dplyr::bind_rows(profiles_ls)
 }
 
-# combine_profiles ----
+# combine_profiles() ----
 combine_profiles <- function(obs_rt_df, model_rt_df) {
   obs_rt_df |>
     dplyr::left_join(
@@ -81,7 +84,7 @@ combine_profiles <- function(obs_rt_df, model_rt_df) {
     dplyr::rename(group = hsagrp, label = hsagrp_label)
 }
 
-# format_profiles_json ----
+# format_profiles_json() ----
 format_profiles_json <- function(df_rts) {
 
   df_json <- df_rts |>
@@ -122,46 +125,3 @@ format_profiles_json <- function(df_rts) {
     )
   )
 }
-
-# review plots ----
-# compare local observed rates with England rates
-# plot_urts <- function(eng_urt, area_urt) {
-#   ggplot2::ggplot() +
-#     ggplot2::geom_line(
-#       ggplot2::aes(x = age, y = urt, group = sex),
-#       color = "#a9adb0",
-#       data = eng_urt
-#     ) +
-#     ggplot2::geom_line(
-#       ggplot2::aes(x = age, y = urt, group = sex, color = sex),
-#       show.legend = FALSE,
-#       data = area_urt
-#     ) +
-#     ggplot2::facet_wrap(ggplot2::vars(hsagrp), scales = "free_y") +
-#     ggplot2::scale_color_manual(values = c("#fd484e", "#2c74b5"))
-# }
-
-# area_codes <- area_codes[!area_codes == "E92000001"]
-
-# plot_ls <- purrr::map2("E92000001", area_codes,
-#   \(x, y) {
-#     i <- urt_dat |> dplyr::filter(area_code == x)
-#     j <- urt_dat |> dplyr::filter(area_code == y)
-#     plot_urts(i, j)
-#   }
-# )
-
-# names(plot_ls) <- area_codes
-
-# walk2(
-#   plot_ls, names(plot_ls),
-#   \(x, y) {
-#     ggplot2::ggsave(
-#       here::here("data", "2022", y, paste0(y, "_review_urt.png")),
-#       x,
-#       width = 400,
-#       height = 300,
-#       units = "mm"
-#     )
-#   }
-# )
---
+++ R/review_activity_inputs.r
@@ -0,0 +1,54 @@
+# README
+# review observed rates by plotting local area rates against England rates
+
+# functions ----
+# plot_rates
+# review_area_obs_rates
+
+# plot_rates() ----
+# helper fn to create a small multiple plot to compare local activity rates
+# against England rates
+# param: obs_eng, type: df, activity rates for England
+# param: obs_area, type: df, activity rates in a local area
+# returns: a small multiple plot, rtype: ggplot2 plot
+plot_rates <- function(obs_eng, obs_area) {
+  ggplot2::ggplot() +
+    ggplot2::geom_line(
+      ggplot2::aes(x = age, y = rt, group = sex),
+      color = "#a9adb0",
+      data = obs_area
+    ) +
+    ggplot2::geom_line(
+      ggplot2::aes(x = age, y = rt, group = sex, color = sex),
+      show.legend = FALSE,
+      data = obs_eng
+    ) +
+    ggplot2::facet_wrap(ggplot2::vars(hsagrp), scales = "free_y") +
+    ggplot2::scale_color_manual(values = c("#fd484e", "#2c74b5"))
+}
+
+# review_area_obs_rates() ----
+# create and save a small multiple plot to compare local activity rates against
+# England rates
+# param: area_code, type: string, ONS geography code
+# param: base_year, type: int, base year for observed rates
+# returns: the filepath for a saved ggplot2 plot, rtype: string
+review_area_obs_rates <- function(area_code, base_year) {
+
+  path_self <- path_closure(area_code, base_year)
+
+  obs_eng <- readr::read_csv(
+    here::here("data", 2022, "E92000001", "obs_rt_df.csv")
+  )
+  obs  <- readr::read_csv(path_self("obs_rt_df.csv"))
+
+  p1 <- plot_rates(obs_eng, obs)
+
+  # save plot
+  filenm <- path_self("review_obs_rates.png")
+  ggplot2::ggsave(
+    filenm, p1, width = 400, height = 300, units = "mm"
+  )
+
+  return(filenm)
+}
