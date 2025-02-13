7c7f4a65109b9fd470ddc13073068366156a045a
paulseamer <paulseamer@nhs.net>
2025-02-13 08:45:16 +0000

    Depend on same codes file called in targets script

--- R/create_area_code_dirs.r
+++ R/create_area_code_dirs.r
@@ -1,17 +1,12 @@
 # README
 # create area code directories
 
-# fetch lists of area codes ----
-df_lad23 <- targets::tar_read(df_raw_lad23)
-df_cty23 <- targets::tar_read(df_raw_cty23)
-df_icb23 <- targets::tar_read(df_icb23)
+# area codes ----
+# areas_path <- here::here("data", "app_input_files", "area_names_and_codes.csv") # nolint: line_length_linter.
 
-lad23 <- unique(df_lad23$lad23cd)
-cty23 <- unique(df_cty23$cty23cd)
-icb23 <- unique(df_icb23$icb23cd)
-eng <- "E92000001"
-
-dirs <- c(lad23, cty23, icb23, eng)
+# dirs <- readr::read_csv(areas_path) |>
+#   dplyr::pull(cd)
 
 # only run once
-purrr::map(dirs, \(x) dir.create(here::here("data", "2022", x)))
+# check 'year' folder exists
+# purrr::map(dirs, \(x) dir.create(here::here("data", "2022", x)))
