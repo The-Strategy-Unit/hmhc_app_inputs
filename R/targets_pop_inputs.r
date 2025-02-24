d036114dcb5a0483a994a2547a238a11dea936c6
paulseamer <paulseamer@nhs.net>
2025-02-24 16:25:56 +0000

    Make fn argument explicit in pipeline

--- R/targets_pop_inputs.r
+++ R/targets_pop_inputs.r
@@ -4,7 +4,7 @@ build_pop_inputs <- list(
   # dynamic branching over row groups (area_code)
   tarchetypes::tar_group_by(
     df_pop_data,
-    build_pop_data(df_mye_series_to100, snpp_series_to100),
+    build_pop_data(df_mye_series_to100, snpp_series_to100, first_proj_yr = 2024),
     area_code
   ),
   tar_target(
